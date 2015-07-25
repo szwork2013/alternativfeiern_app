d222e97f9a7cb598c4157678d8765093df96d482
Scarysize <znarf92@gmx.net>
2015-07-25 16:44:11 +0200

    new structure

---
+++ AF_Backend/af_backend/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url' : 'mongodb://localhost/af_backend_users'
+}
---
+++ AF_Backend/af_backend/views/signup.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var DefaultLayout = require('./layouts/default')
+
+var Signup = React.createClass({
+
+  render: function() {
+    return (
+      <DefaultLayout>
+        <form action="/signup" method="post">
+          <div class="form-group">
+              <label>Email</label>
+              <input type="text" class="form-control" name="email" />
+          </div>
+          <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control" name="password" />
+          </div>
+
+          <button type="submit" class="btn btn-warning btn-lg">Signup</button>
+        </form>
+      </DefaultLayout>
+    );
+  }
+
+});
+
+module.exports = Signup;
